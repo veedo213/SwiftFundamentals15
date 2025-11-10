2acf842ac8843ac9231d4f46d9a53e1f6a0396c0
veedo213 <vyngocdo2002@gmail.com>
2025-11-09 23:34:13 -0500

    Guider Project Light 1_9

---
+++ Unit1/Lesson1_9/InterfaceBuilderBasics/InterfaceBuilderBasics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterfaceBuilderBasics
+//
+//  Created by Christian Tran on 11/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Unit1/Lesson1_9/InterfaceBuilderBasics/InterfaceBuilderBasicsTests/InterfaceBuilderBasicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceBuilderBasicsTests.swift
+//  InterfaceBuilderBasicsTests
+//
+//  Created by Christian Tran on 11/9/25.
+//
+
+import Testing
+@testable import InterfaceBuilderBasics
+
+struct InterfaceBuilderBasicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
