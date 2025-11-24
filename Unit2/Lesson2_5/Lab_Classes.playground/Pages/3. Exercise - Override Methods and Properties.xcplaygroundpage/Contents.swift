16d1206628f39b399530572a2300f7af80fc3537
veedo213 <vyngocdo2002@gmail.com>
2025-11-23 22:42:59 -0500

    COMPLETED LESSON 2_5

--- Unit2/Lesson2_5/Lab_Classes.playground/Pages/3. Exercise - Override Methods and Properties.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_5/Lab_Classes.playground/Pages/3. Exercise - Override Methods and Properties.xcplaygroundpage/Contents.swift
@@ -37,13 +37,76 @@ class Fighter: Spaceship {
     }
 }
 //:  Define a new class `ShieldedShip` that inherits from `Fighter`. Add a variable property `shieldStrength` that defaults to 25. Create a new instance of `ShieldedShip` called `defender`. Set `name` to "Defender" and `weapon` to "Cannon." Call `moveRight()` and print `position`, then call `fire()` and print `remainingFirePower`.
+class ShieldedShip: Fighter {
+    var shieldStrength = 25
 
+// Optionally, you could override wasHit for custom shield behavior, but not required for this exercise.
+
+}
+
+// Create an instance of ShieldedShip
+let defender = ShieldedShip()
+defender.name = "Defender"
+defender.weapon = "Cannon"
+
+// Move right once, print position
+defender.moveRight()
+print(defender.position) // Output: 1
+
+// Fire once, print remainingFirePower
+defender.fire()
+print(defender.remainingFirePower) // Output: 4
 
 //:  Go back to your declaration of `ShieldedShip` and override `wasHit()`. In the body of the method, check to see if `shieldStrength` is greater than 0. If it is, decrement `shieldStrength` by 5. Otherwise, decrement `health` by 5. Call `wasHit()` on `defender` and print `shieldStrength` and `health`.
+class ShieldedShip: Fighter {
+    var shieldStrength = 25
 
+override func wasHit() {
+    if shieldStrength > 0 {
+        shieldStrength -= 5
+    } else {
+        health -= 5
+        if health <= 0 {
+            print("Sorry, your ship was hit one too many times. Do you want to play again?")
+        }
+    }
+}
+
+}
+
+// Create an instance of ShieldedShip
+let defender = ShieldedShip()
+defender.name = "Defender"
+defender.weapon = "Cannon"
+
+// Call wasHit() and print shieldStrength and health
+defender.wasHit()
+print(defender.shieldStrength) // Output: 20
+print(defender.health)         // Output: 100
 
 //:  When `shieldStrength` is 0, all `wasHit()` does is decrement `health` by 5. That's exactly what the implementation of `wasHit()` on `Spaceship` does! Instead of rewriting that, you can call through to the superclass implementation of `wasHit()`. Go back to your implementation of `wasHit()` on `ShieldedShip` and remove the code where you decrement `health` by 5 and replace it with a call to the superclass's implementation of the method. Call `wasHit()` on `defender`, then print `shieldStrength` and `health`.
+class ShieldedShip: Fighter {
+    var shieldStrength = 25
 
+override func wasHit() {
+    if shieldStrength > 0 {
+        shieldStrength -= 5
+    } else {
+        super.wasHit()
+    }
+}
+
+}
+
+// Create an instance of ShieldedShip
+let defender = ShieldedShip()
+defender.name = "Defender"
+defender.weapon = "Cannon"
+
+// Call wasHit() and print shieldStrength and health
+defender.wasHit()
+print(defender.shieldStrength) // Output: 20
+print(defender.health)         // Output: 100
 
 /*:
 [Previous](@previous)  |  page 3 of 4  |  [Next: Exercise - Class Memberwise Initializers and References](@next)
--- Unit2/Lesson2_5/Lab_Classes.playground/Pages/4. Exercise - Class Memberwise Initializers and References.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_5/Lab_Classes.playground/Pages/4. Exercise - Class Memberwise Initializers and References.xcplaygroundpage/Contents.swift
@@ -53,24 +53,115 @@ class ShieldedShip: Fighter {
 
  Then create an instance of `Spaceship` below called `falcon`. Use the memberwise initializer you just created. The ship's name should be "Falcon."
  */
+class Spaceship {
+    let name: String
+    var health: Int
+    var position: Int
 
+// Custom initializer for all properties
+init(name: String, health: Int, position: Int) {
+    self.name = name
+    self.health = health
+    self.position = position
+}
 
+func moveLeft() {
+    position -= 1
+}
+
+func moveRight() {
+    position += 1
+}
+
+func wasHit() {
+    health -= 5
+    if health <= 0 {
+        print("Sorry, your ship was hit one too many times. Do you want to play again?")
+    }
+}
+
+}
+
+// Create Spaceship instance called falcon using the new initializer
+let falcon = Spaceship(name: "Falcon", health: 100, position: 0)
 /*:
  Writing initializers for subclasses can get tricky. Your initializer needs to not only set the properties declared on the subclass, but also set all of the uninitialized properties on classes that it inherits from. Go to the declaration of `Fighter` and write an initializer that takes an argument for each property on `Fighter` and for each property on `Spaceship`. Set the properties accordingly. (Hint: you can call through to a superclass's initializer with `super.init` *after* you initialize all of the properties on the subclass).
 
  Then create an instance of `Fighter` below called `destroyer`. Use the memberwise initializer you just created. The ship's name should be "Destroyer."
  */
+class Fighter: Spaceship {
+    let weapon: String
+    var remainingFirePower: Int
 
+// Custom initializer for Fighter (and inherited Spaceship properties)
+init(name: String, health: Int, position: Int, weapon: String, remainingFirePower: Int) {
+    self.weapon = weapon
+    self.remainingFirePower = remainingFirePower
+    super.init(name: name, health: health, position: position)
+}
 
+func fire() {
+    if remainingFirePower > 0 {
+        remainingFirePower -= 1
+    } else {
+        print("You have no more fire power.")
+    }
+}
+
+}
+
+// Creating an instance of Fighter called destroyer
+let destroyer = Fighter(name: "Destroyer", health: 80, position: 5, weapon: "Laser", remainingFirePower: 10)
 /*:
  Now go add an initializer to `ShieldedShip` that takes an argument for each property on `ShieldedShip`, `Fighter`, and `Spaceship`, and sets the properties accordingly. Remember that you can call through to the initializer on `Fighter` using `super.init`.
 
  Then create an instance of `ShieldedShip` below called `defender`. Use the memberwise initializer you just created. The ship's name should be "Defender."
  */
+class ShieldedShip: Fighter {
+    var shieldStrength: Int
 
+// Custom initializer for ShieldedShip (plus inherited properties)
+init(name: String, health: Int, position: Int, weapon: String, remainingFirePower: Int, shieldStrength: Int) {
+    self.shieldStrength = shieldStrength
+    super.init(name: name, health: health, position: position, weapon: weapon, remainingFirePower: remainingFirePower)
+}
 
+override func wasHit() {
+    if shieldStrength > 0 {
+        shieldStrength -= 5
+    } else {
+        super.wasHit()
+    }
+}
+
+}
+
+// Creating an instance of ShieldedShip called defender
+let defender = ShieldedShip(name: "Defender", health: 120, position: 10, weapon: "Plasma", remainingFirePower: 15, shieldStrength: 25)
 //:  Create a new constant named `sameShip` and set it equal to `falcon`. Print out the position of `sameShip` and `falcon`, then call `moveLeft()` on `sameShip` and print out the position of `sameShip` and `falcon` again. Did both positions change? Why? If both were structs instead of classes, would it be the same? Why or why not? Provide your answer in a comment or print statement below.
+swift
+let sameShip = falcon
+// sameShip and falcon now both reference the same Spaceship object in memory.
 
+print("Before moving left: sameShip.position =", sameShip.position, ", falcon.position =", falcon.position)
+// Output: Before moving left: sameShip.position = 0 , falcon.position = 0
+// Both show the same value, since they're the same object.
+
+sameShip.moveLeft()
+// This decreases the position of the ship by 1.
+// Because sameShip and falcon reference the same object, both will now show the updated position.
+
+print("After moving left: sameShip.position =", sameShip.position, ", falcon.position =", falcon.position)
+// Output: After moving left: sameShip.position = -1 , falcon.position = -1
+// Both changed! Any change through one reference is visible through the other.
+
+/// Explanation:
+print("""
+Did both positions change? Yes. This is because in Swift, classes are reference types.
+When you assign one class instance to another variable or constant, both refer to the same single instance in memory.
+If Spaceship were a struct (value type) instead, assigning falcon to sameShip would create a new copy.
+In that case, changing sameShip would NOT affect falcon — they'd each keep their own separate values.
+""")
 
 /*:
  _Copyright © 2023 Apple Inc._
