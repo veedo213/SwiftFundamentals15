bfea123c0fefd33cef94f0641ee0de983573d7f2
veedo213 <vyngocdo2002@gmail.com>
2025-11-23 23:10:10 -0500

    COMPLETED LESSON 2_6

--- Unit2/Lesson2_6/Lab_Collections.playground/Pages/1. Exercise - Arrays.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_6/Lab_Collections.playground/Pages/1. Exercise - Arrays.xcplaygroundpage/Contents.swift
@@ -3,22 +3,20 @@
  
  Assume you are an event coordinator for a community charity event and are keeping a list of who has registered. Create a variable `registrationList` that will hold strings. It should be empty after initialization.
  */
-
-
+var registrationList: [String] = []// registrationList should be an empty array of Strings after initialization
 //:  Your friend Jasmine is the first to register for the event. Add her name to `registrationList` using the `append(_:)` method. Print the contents of the collection.
-
+registrationList.append("Jasmine")// Jasmine is the first to registerprint(registrationList)// Print the contents: ["Jasmine"]
 
 //:  Add four additional names into the array using the `+=` operator. All of the names should be added in one step. Print the contents of the collection.
-
+registrationList += ["Miguel", "Amina", "Priya", "Sophie"]// Add four more names in one stepprint(registrationList)// Print the contents: ["Jasmine", "Miguel", "Amina", "Priya", "Sophie"]
 
 //:  Use the `insert(_:at:)` method to add `Trey` into the array as the second element. Print the contents of the collection.
-
-
+registrationList.insert("Trey", at: 1)// Insert Trey as the second elementprint(registrationList)// Print the contents: ["Jasmine", "Trey", "Miguel", "Amina", "Priya", "Sophie"]
 //:  Somebody had a conflict and decided to transfer registration to someone else. Use array subscripting to change the sixth element to `Elton`. Print the contents of the collection.
-
+registrationList[5] = "Elton"// Change the sixth registrant to Eltonprint(registrationList)// Print the contents: ["Jasmine", "Trey", "Miguel", "Amina", "Priya", "Elton"]
 
 //:  Call `removeLast()` on `registrationList`. If done correctly, this should remove `Elton` from the collection. Store the result of `removeLast()` into a new constant `deletedItem`, then print `deletedItem`.
-
+let deletedItem = registrationList.removeLast()// Remove the last registrant and store the resultprint(deletedItem)// Print: "Elton"
 
 /*:
 page 1 of 4  |  [Next: App Exercise - Activity Challenge](@next)
--- Unit2/Lesson2_6/Lab_Collections.playground/Pages/2. App Exercise - Activity Challenge.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_6/Lab_Collections.playground/Pages/2. App Exercise - Activity Challenge.xcplaygroundpage/Contents.swift
@@ -7,19 +7,33 @@
  
  Using arrays of type `String`, create at least two lists, one for walking challenges, and one for running challenges. Each should have at least two challenges and should be initialized using an array literal. Feel free to create more lists for different activities.
  */
+let walkingChallenges = ["Walk 3 miles a day", "Take 10,000 steps"]
+let runningChallenges = ["Run 5 times a week", "Complete a 10k"]
 
+// Optionally, here’s a third for variety:
+let calisthenicsChallenges = ["50 push-ups every day", "Plank for 5 minutes"]
 
 //:  In your app you want to show all of these lists on the same screen grouped into sections. Create a `challenges` array that holds each of the lists you have created (it will be an array of arrays). Using `challenges`, print the first element in the second challenge list.
-
+// Group all activity challenge lists into a single array of arrayslet challenges = [walkingChallenges, runningChallenges, calisthenicsChallenges]// Print the first element in the second challenge list (which is runningChallenges)print(challenges[1][0])// Output: Run 5 times a week
 
 //:  All of the challenges will reset at the end of the month. Use the `removeAll` to remove everything from `challenges`. Print `challenges`.
-
+var mutableChallenges = challenges // Make a mutable copy since 'let' arrays are immutable
+mutableChallenges.removeAll()
+print(mutableChallenges) // Output: []
 
 //:  Create a new array of type `String` that will represent challenges a user has committed to instead of available challenges. It can be an empty array or have a few items in it.
-
+var committedChallenges: [String] = []// Uncomment below to test with one challenge committed
+// committedChallenges = ["Run 5 times a week"]// Uncomment below to test with multiple challenges committed
+// committedChallenges = ["Run 5 times a week", "Ten thousand steps"]
 
 //:  Write an if statement that will use `isEmpty` to check if there is anything in the array. If there is not, print a statement asking the user to commit to a challenge. Add an else-if statement that will print "The challenge you have chosen is <INSERT CHOSEN CHALLENGE>" if the array count is exactly 1. Then add an else statement that will print "You have chosen multiple challenges."
-
+if committedChallenges.isEmpty {
+    print("Please commit to a challenge.")
+} else if committedChallenges.count == 1 {
+    print("The challenge you have chosen is (committedChallenges[0])")
+} else {
+    print("You have chosen multiple challenges.")
+}
 
 /*:
 [Previous](@previous)  |  page 2 of 4  |  [Next: Exercise - Dictionaries](@next)
